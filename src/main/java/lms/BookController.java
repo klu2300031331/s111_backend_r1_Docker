b6028b384fc02040f6e71fe7e4304af89e394520
Thota Gowtham <163727915+klu2300031331@users.noreply.github.com>
2025-11-05 09:18:25 +0530

    Update BookController.java

--- src/main/java/lms/BookController.java
+++ src/main/java/lms/BookController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/back1/api/books")
 @CrossOrigin(origins = "*") // Allow frontend to call API
 public class BookController {
 
